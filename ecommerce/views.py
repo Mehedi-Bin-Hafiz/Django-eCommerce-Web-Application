440d8db45ef13ea269bf6b8cd7bd4a344e9d6093
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-10-21 10:26:53 +0600

    update content

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,18 +3,21 @@ from django.http import HttpResponse
 
 def home_page(request):
     context = {
-        "title" : " Hello Mehedi"
+        "title" : " Hello home",
+        "content" : "This is homepage"
     }
     return  render(request, "home_page.html",context) #render is a shortcut that take a requst and return template file {} is a context
 
 def about_page(request):
     context = {
-        "title": " Hello about "
+        "title": " Hello about ",
+        "content": "This is about page"
     }
     return  render(request, "home_page.html",context)
 
 def contact_page(request):
     context = {
-        "title": " Hello contact "
+        "title": " Hello contact ",
+        "content": "This is contact page"
     }
     return  render(request, "home_page.html",context)
