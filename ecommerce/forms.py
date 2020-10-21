d175bde3c302dcdb0309f356aefa39910cb19575
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-10-21 14:34:31 +0600

    Example of django form

---
+++ ecommerce/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(widget = forms.TextInput(
+        attrs={
+            "class": "form-control","placeholder":"enter fullname", "id": "form_full_name", "name":"name"
+        })
+    )
+
+    #here fullname act as name in html
+    email = forms.EmailField(widget = forms.EmailInput(
+        attrs={
+            "class": "form-control","placeholder":"enter email", "id": "form_full_email","name":"email"
+        }))
+    content = forms.CharField(widget = forms.Textarea(
+        attrs={
+            "class": "form-control","placeholder":"enter content", "id": "form_full_content", "name":"content"
+        }))
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import ContactForm
+from django import forms
 def home_page(request):
     context = {
         "title" : " Hello home",
@@ -16,12 +17,26 @@ def about_page(request):
     return  render(request, "home_page.html",context)
 
 def contact_page(request):
+
+    contact_form = ContactForm(request.POST or None) #it post then pass or not
+    if contact_form.is_valid():
+
+        print("Cleaned data",contact_form.cleaned_data)
+
     context = {
         "title": " Hello contact ",
-        "content": "This is contact page"
+        "content": "This is contact page",
+        "form": contact_form
     }
-    if request.method ==  "POST":
-        print(request.POST.get("name"))
-        print(request.POST.get("email"))
-        print(request.POST.get("content"))
+    # if request.method ==  "POST":
+        # print(request.POST.get("fullname"))
+        # print(request.POST.get("email"))
+        # print(request.POST.get("content"))
     return  render(request, "contact/view.html",context)
+
+##validate
+def clean_email(self):
+    email = self.cleand_data.get('email')
+    if not "@gmail.com" in email:
+        raise forms.ValidationError("enter valid gmail")
+    return email
