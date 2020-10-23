1fb7109b7c350da725e82af2c19a665e28997d37
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-10-23 11:36:47 +0600

    Creating my first model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=120) #charfield must need max_length
+    description = models.TextField()
+    price = models.DecimalField(max_digits=20,decimal_places=20,null=True) #as we add later so we need to add null = True of set default value.
+
