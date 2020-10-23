e1bd5165858cc5a7296952313f410c89b15b2495
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-10-23 12:00:58 +0600

    register my model in admin site

--- products/models.py
+++ products/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Product(models.Model):
-    title = models.CharField(max_length=120) #charfield must need max_length
-    description = models.TextField()
-    price = models.DecimalField(max_digits=20,decimal_places=20,null=True) #as we add later so we need to add null = True of set default value.
+    title           = models.CharField(max_length=120) #charfield must need max_length
+    description     = models.TextField()
+    price           = models.DecimalField(max_digits=20,decimal_places=2,null=True) #as we add later so we need to add null = True of set default value.
 
+    def __str__(self):# str is a function that overwrite class object in database
+        return self.title
